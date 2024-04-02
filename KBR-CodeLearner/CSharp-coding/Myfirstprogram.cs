bee73c0f353cc588f91c8c682e98f9c30cb5b042
Naveen kumar <Naveen kumar@LAPTOP-K50CHIJR>
2024-04-02 19:42:06 +0530

    My first program

---
+++ KBR-CodeLearner/CSharp-coding/Myfirstprogram.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KBR_CodeLearner.CSharp_coding
+{
+   public class Myfirstprogram
+    {
+        string color = "red";
+        static void main(string[] arg)
+        {
+            Myfirstprogram mycar = new Myfirstprogram();
+            Console.WriteLine(mycar.color);
+        }
+    }
+}
